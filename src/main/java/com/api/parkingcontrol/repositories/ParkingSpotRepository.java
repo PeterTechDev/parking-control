b3cd632bad1a8e11b50dbdc40c33020948bd2c43
petertechdev <peterleite.dev@gmail.com>
2022-08-23 06:22:43 -0300

    create bean ParkingSpotService

--- src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
+++ src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-//teste
+
 @Repository
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
 }
